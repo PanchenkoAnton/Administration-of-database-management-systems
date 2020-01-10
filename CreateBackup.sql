5d04a68e42f9b151ec0941e253ad1ca54172df4f
PanchenkoAnton <panchenkoantonvl@gmail.com>
2020-01-10 10:16:23 +1000

    upgrade

--- CreateBackup.sql
+++ CreateBackup.sql
@@ -6,40 +6,40 @@ CREATE PROCEDURE CreateBackup
 (@dbName NVARCHAR(50), @isFull BIT)
 AS
 BEGIN
-	DECLARE @BackupPath NVARCHAR(100), @CurrentDate NVARCHAR(20)
+	DECLARE @BackupPath NVARCHAR(100), @CurrentDate NVARCHAR(20), @Date DATETIME
+	SET @Date = GETDATE()
 	SET @CurrentDate = (
 		SELECT
 		CONCAT_WS(
 			'T',
 			CONCAT_WS(
 				'-',
-				RIGHT(DATEPART(YEAR  , GETDATE()), 4),
-				RIGHT(DATEPART(MONTH, GETDATE()), 2),
-				RIGHT(DATEPART(DAY, GETDATE()), 2)
+				RIGHT(DATEPART(YEAR  , @Date), 4),
+				RIGHT(DATEPART(MONTH, @Date), 2),
+				RIGHT(DATEPART(DAY, @Date), 2)
 			),
 			CONCAT_WS(
 				'.',
-				RIGHT(DATEPART(HOUR  , GETDATE()), 2),
-				RIGHT(DATEPART(MINUTE, GETDATE()), 2),
-				RIGHT(DATEPART(SECOND, GETDATE()), 2)
+				RIGHT(DATEPART(HOUR  , @Date), 2),
+				RIGHT(DATEPART(MINUTE, @Date), 2),
+				RIGHT(DATEPART(SECOND, @Date), 2)
 			)
 		)
 	)
 -- (SELECT CONVERT(VARCHAR(20), GETDATE(), 126))
 	IF @isFull = 1
-		BEGIN
-			SET @BackupPath = 'D:\tmp_backups\' + @dbName + '_full_' + @CurrentDate + '.bak'
-			BACKUP DATABASE @dbName
-			TO
-			DISK = @BackupPath
-			WITH INIT;
-		END
+	BEGIN
+		SET @BackupPath = 'D:\tmp_backups\' + @dbName + '_full_' + @CurrentDate + '.bak'
+		BACKUP DATABASE @dbName
+		TO DISK = @BackupPath
+		WITH INIT;
+	END
 	ELSE
-		BEGIN
-			SET @BackupPath = 'D:\tmp_backups\' + @dbName + '_diff_' + @CurrentDate + '.bak'
-			BACKUP DATABASE @dbName
-			TO
-			DISK = @BackupPath
-			WITH DIFFERENTIAL, INIT;
-		END
+	BEGIN
+		SET @BackupPath = 'D:\tmp_backups\' + @dbName + '_diff_' + @CurrentDate + '.bak'
+		BACKUP DATABASE @dbName
+		TO DISK = @BackupPath
+		WITH DIFFERENTIAL, INIT;
+	END
+	INSERT INTO [dbo].[backup_logs] VALUES (@isFull, @Date, @dbName)
 END
--- RestoreBackup.sql
+++ RestoreBackup.sql
@@ -2,15 +2,50 @@ USE master;
 GO
 DROP PROCEDURE IF EXISTS RestoreBackup
 GO
+/*ALTER DATABASE SampleDB
+SET SINGLE_USER
+WITH ROLLBACK IMMEDIATE
+GO*/
 CREATE PROCEDURE RestoreBackup
-(@dbName NVARCHAR(50), @Date NVARCHAR(100))
+(@dbName NVARCHAR(50), @fullDate NVARCHAR(100), @diffDate NVARCHAR(100), @isFull BIT)
 AS
 BEGIN
-	DECLARE @BackupPath NVARCHAR(100)
-	SET @BackupPath = 'D:\tmp_backups\' + @dbName + '_full_' + @Date + '.bak'
+	DECLARE @fullBackupPath NVARCHAR(100), @diffBackupPath NVARCHAR(100)
+	SET @fullBackupPath = 'D:\tmp_backups\' + @dbName + '_full_' + @fullDate + '.bak'
+	SET @diffBackupPath = 'D:\tmp_backups\' + @dbName + '_diff_' + @diffDate + '.bak'
+	--SET @logBackupPath = 'D:\tmp_backups\' + @dbName + '.TRN'
+	/*BACKUP LOG @dbName 
+	TO DISK = 'D:\tmp_backups\mylog'
+	WITH NORECOVERY;
 	RESTORE DATABASE @dbName
 	FROM
-	DISK = @BackupPath
+	DISK = @fullBackupPath
 	WITH
-	NORECOVERY;
+	RECOVERY;*/
+	IF @isFull = 1
+	BEGIN
+		BACKUP LOG @dbName 
+		TO DISK = 'D:\tmp_backups\mylog'
+		WITH NORECOVERY;
+		RESTORE DATABASE @dbName
+		FROM DISK = @fullBackupPath
+		WITH RECOVERY;
+	END
+	ELSE
+	BEGIN
+		BACKUP LOG @dbName 
+		TO DISK = 'D:\tmp_backups\mylog'
+		WITH NORECOVERY;
+		RESTORE DATABASE @dbName
+		FROM DISK = @fullBackupPath
+		WITH NORECOVERY;
+		RESTORE DATABASE @dbName
+		FROM DISK = @diffBackupPath
+		WITH RECOVERY;
+	END
 END
+GO
+/*ALTER DATABASE SampleDB
+SET MULTI_USER
+WITH NO_WAIT
+GO*/
